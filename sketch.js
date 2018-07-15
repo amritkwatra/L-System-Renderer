ffeccaec026ac95eadf4cfffaa6d514fd0122b63
Amrit <ak2244@cornell.edu>
2018-07-15 02:55:37 -0400

    deeper tree with randomized/alternate path branches

--- sketch.js
+++ sketch.js
@@ -1,18 +1,20 @@
+let translation;
+let lsystem = new LSystem(250, 500);
+
 function setup() {
     angleMode(DEGREES);
-    createCanvas(500, 500);
+    createCanvas(1000, 500);
     background(0);
     stroke(126, 222, 229);
     strokeWeight(1);
-    let lsystem = new LSystem(250, 450);
-    lsystem.setLength(5);
+    lsystem.setLength(2.5);
     lsystem.setRotationAngle(-25);
     lsystem.setTheta(-90);
     lsystem.addTranslation("F", ["FF"]);
     lsystem.addTranslation("X", ["F+[[X]-X]-F[-FX]+X", "F-[[X]+X]+F[+FX]-X"]);
     lsystem.defineAxiom("X");
-    let transOut = lsystem.translateAxiom(5);
-    lsystem.drawString(transOut);
+    translation = lsystem.translateAxiom(6);
+    lsystem.drawString(translation);
 }
 
 function draw() {}
